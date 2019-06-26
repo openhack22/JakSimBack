ca1b03516ea600a72b31e3d9a89cc67f3a1305f3
tmrl0419 <them0419@naver.com>
2019-06-27 03:59:52 +0900

    Final first day coding

--- app.py
+++ app.py
@@ -1,44 +1,82 @@
-from flask import Flask,request;
+from flask import Flask,request, session
+import src.sql_command as db
 import json
+from datetime import timedelta
 
 app = Flask(__name__)
-
+app.secret_ket = 'JaksimForever'
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route("/regist", methods = ['POST'])    # 회원가입.
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=60)
+
+
+@app.route("/regist", methods = ['POST'])
 def regist():
-    user = request.form['user']                     # 아이디 중복 체크 넣어야함
+    """Regist ID Form"""
+    id = request.form['id']
     password = request.form['password']
-    name = request.form['name']
+    username = request.form['username']
     #print(request.form)
-    #result = DBsetting.user_regist(user,password,name)
-    #jsonresult = {
-    #    'result' : result
-    #}
-    #resJson = json.dumps(jsonresult)
-    #return resJson
+    resDB = db.userRegist(id,password,username)
+    if resDB == 200:
+        session[id] = id
+    jsonresult = {
+        'result' : resDB
+    }
+    resJson = json.dumps(jsonresult)
+    return resJson
 
-@app.route("/login", methods = ['POST'])    # 로그인.
+
+@app.route("/login", methods = ['POST'])
 def login():
-    user = request.form['user']
+    """Login Form"""
+    id = request.form['id']
     password = request.form['password']
     #print(request.form)
-    #userid = DBsetting.user_login(user,password)
-    #jsonresult = {
-    #    'id' : userid
-    #}
-    #jsonString = json.dumps(jsonresult)
-    #print (jsonString)
-    #return jsonString
+    resDB = db.userLogin(id,password)
+
+    if resDB == 200:
+        session[id] = id
+
+    jsonResult = {
+        'result' : resDB
+    }
+    jsonString = json.dumps(jsonResult)
+    print (jsonString)
+    return jsonString
 
 
+@app.route("/logout")
+def logout():
+    """Logout Form"""
+    id = request.form[id]
+    session.pop(id,None)
+    return 200
 
 
+@app.route("/getList")
+def getGoalList():
+    """get Goal List Form"""
+    id = request.form[id]
+    if not session.get(id):
+        jsonResult = {
+            'result': 401
+        }
+        jsonString = json.dumps(jsonResult)
+        return jsonString
+    resDB = db.getOnGoingList(id)
+    jsonResult = {
+        'result': resDB
+    }
 
+    #return resJson
 
 
 if __name__ == '__main__':
