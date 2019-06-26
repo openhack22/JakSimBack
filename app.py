e31babee1541115a10c53b97c267f5ec8cd34784
tmrl0419 <them0419@naver.com>
2019-06-27 01:43:20 +0900

    add login, register func

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,request;
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,38 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route("/regist", methods = ['POST'])    # 회원가입.
+def regist():
+    user = request.form['user']                     # 아이디 중복 체크 넣어야함
+    password = request.form['password']
+    name = request.form['name']
+    #print(request.form)
+    #result = DBsetting.user_regist(user,password,name)
+    #jsonresult = {
+    #    'result' : result
+    #}
+    #resJson = json.dumps(jsonresult)
+    #return resJson
+
+@app.route("/login", methods = ['POST'])    # 로그인.
+def login():
+    user = request.form['user']
+    password = request.form['password']
+    #print(request.form)
+    #userid = DBsetting.user_login(user,password)
+    #jsonresult = {
+    #    'id' : userid
+    #}
+    #jsonString = json.dumps(jsonresult)
+    #print (jsonString)
+    #return jsonString
+
+
+
+
+
 
 
 if __name__ == '__main__':
